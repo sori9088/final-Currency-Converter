c418c3b526db99f2363ae24c7d718cc12c58c9f6
Hansol <sori9088@naver.com>
2019-10-01 16:39:52 +0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+const exchangeRates = {
+    "usd": {
+      "eur": 0.91,
+      "aud": 1.48,
+      "krw": 1203.00,
+      "vnd": 23200.70,
+    },
+    "eur": {
+      "usd": 1.09,
+      "aud": 1.62 ,
+      "krw": 1316.21,
+      "vnd": 25383.96,
+    },
+    "aud": {
+      "usd": 0.68,
+      "eur": 0.62,
+      "krw": 813.58,
+      "vnd": 15690.63,
+    },
+    "krw": {
+      "usd": 0.00083,
+      "aud": 0.0012,
+      "eur": 0.00076,
+      "vnd": 19.29,
+    },
+    "vnd": {
+      "krw": 0.052,
+      "usd": 0.000043,
+      "aud": 0.000064,
+      "eur": 0.000039,
+    },
+  }
+
+let amount= document.getElementById("amountTextBox");
+let covertButton= document.getElementById("convertButton");
+let resultArea= document.getElementById("resultArea");
+let fromSelector= document.getElementById("fromCurrency");
+let toSelector=document.getElementById("toCurrency");
+
+
+covertButton.addEventListener("click",convert);
+
+function formatCurrency(type, value) {
+    const formatter = new Intl.NumberFormat(type, {
+      currency: type,
+      style: "currency"
+    });
+    return formatter.format(value);
+  }
+
+  let fromCurrency;
+  let toCurrency;
+  let money;
+
+function convert(){
+    money = amount.value;
+    fromCurrency= fromSelector[fromSelector.selectedIndex].value;
+    toCurrency= toSelector[toSelector.selectedIndex].value;
+    callApi(fromCurrency,toCurrency);
+    result = exchangeRates[fromCurrency.toLowerCase()][toCurrency.toLowerCase()]*money;
+    resultArea.innerHTML = formatCurrency(toCurrency,result);
+
+}
+
+async function callApi(fromCurrency,toCurrency) {
+    let currency= fromCurrency+"_"+toCurrency;
+    console.log(currency);
+    let url = ('https://free.currencyconverterapi.com/api/v6/convert?q=' + currency + '&compact=ultra&apiKey=02090325190c07d6148f');
+    let resultapi = await fetch(url);
+    let json = await resultapi.json();
+    let rate = json[currency];
+    console.log("this is the rate",rate);
+    let exchangedAmount = rate*money;
+    console.log ("converted amount is ",exchangedAmount);
+    updateResults(json);
+}
+
+function updateResults(response) {
+  console.log(response);
+}
+
